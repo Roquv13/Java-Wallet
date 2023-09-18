24205bcbfd2eea4eaca46e1562aac78c1c72bc5e
Cezary Olszewski <czarekolszewski99@gmail.com>
2023-09-18 12:26:47 +0200

    Add methods init and usrAmount and class currency

--- main.java
+++ main.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 class AmountChange {
     float amountUser;
-
+    Scanner sc = new Scanner(System.in);
+    public void init() {
+        System.out.println("Add your initial amount of wallet: ");
+        amountUser = sc.nextFloat();
+    }
     public void add(float amount) {
         amountUser = amountUser + amount;
         System.out.println("Amount added: " + amount);
@@ -10,9 +16,52 @@ class AmountChange {
         amountUser = amountUser - amount;
         System.out.println("Amount removed: " + amount);
     }
+
+    public void usrAmount() {
+        System.out.println("Amount on user account: " + amountUser);
+    }
+}
+
+class Currency {
+    float amountUser;
+    float amountCurrency;
+    public void usd(float currency) {
+        currency = 1/currency;
+        amountCurrency = amountUser * currency;
+        System.out.println("Value of wallet in USD: " + amountCurrency);
+    }
+
+    public void euro(float currency) {
+        currency = 1/currency;
+        amountCurrency = amountUser * currency;
+        System.out.println("Value of wallet in EURO: " + amountCurrency);
+    }
+
+    public void chf(float currency) {
+        currency = 1/currency;
+        amountCurrency = amountUser * currency;
+        System.out.println("Value of wallet in CHF: " + amountCurrency);
+    }
 }
 public class main {
     public static void main(String[] args) {
+        AmountChange amoChg = new AmountChange();
+        Scanner sc = new Scanner(System.in);
 
+        amoChg.init();
+        System.out.println("Do you want to add more money to wallet? ");
+        System.out.println("1. Yes");
+        System.out.println("2. No");
+        int userAns = sc.nextInt();
+        if (userAns == 1) {
+            System.out.println("Add amount to wallet: ");
+            float amountToAdd = sc.nextFloat();
+            amoChg.add(amountToAdd);
+        } else if (userAns == 2) {
+            System.out.println("Remove amount from wallet: ");
+            float amountToRemove = sc.nextFloat();
+            amoChg.remove(amountToRemove);
+        } else System.out.println("Type again!");
+        amoChg.usrAmount();
     }
 }
---
+++ out/production/Java-Wallet/README.md
@@ -0,0 +1,20 @@
+# Java-Wallet
+Creating a wallet application in Java
+
+1. **User Registration**:
+   - Create a registration process for new users to sign up for your wallet application. User information, verifying email addresses, and setting up initial account details.
+
+2. **User Profiles**:
+   - Allow users to manage their profiles, including updating personal information, changing passwords, and setting preferences.
+
+3. **Wallet Creation**:
+   - Users should be able to create multiple wallets for different currencies (e.g., USD, EUR).
+
+4. **Balances and Transactions**:
+   - Implement functionality to view wallet balances, transaction history, and the ability to send/receive funds.
+
+5. **Transaction History**:
+   - Store and display transaction history, including transaction details, dates, and confirmations.
+
+6. **Currency Conversion**:
+   - If your wallet supports multiple currencies, implement currency conversion functionality to display balances and transaction amounts in the user's preferred currency.

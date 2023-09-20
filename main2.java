46f4a1f2e2c59168d8121720d48ce2a14564244a
Cezary Olszewski <czarekolszewski99@gmail.com>
2023-09-20 12:53:26 +0200

    Add some changes to class CurrencyConverter

--- main2.java
+++ main2.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 class Amount {
@@ -28,29 +30,36 @@ class CurrencyConverter {
     float usdCurrency;
     float euroCurrency;
     float chfCurrency;
-    float amountUsd;
-    float amountEuro;
-    float amountChf;
+    BigDecimal amountUsd;
+    BigDecimal amountEuro;
+    BigDecimal amountChf;
     
-    public float usd(float amount) {
+    public void list() {
+        System.out.println("List of actual currencies");
+        System.out.println("1. USD");
+        System.out.println("2. EURO");
+        System.out.println("3. CHF");
+        System.out.println("4. Exit");
+    }
+    public BigDecimal usd(float amount) {
         System.out.println("Type actual currency of USD: ");
         usdCurrency = sc.nextFloat();
         usdCurrency = 1/usdCurrency;
-        amountUsd = amount * usdCurrency;
+        amountUsd = BigDecimal.valueOf(amount * usdCurrency).setScale(2, RoundingMode.HALF_UP);
         return amountUsd;
     }
-    public float euro(float amount) {
+    public BigDecimal euro(float amount) {
         System.out.println("Type actual currency of EURO: ");
         euroCurrency = sc.nextFloat();
         euroCurrency = 1/euroCurrency;
-        amountEuro = amount * euroCurrency;
+        amountEuro = BigDecimal.valueOf(amount * euroCurrency).setScale(2, RoundingMode.HALF_UP);
         return amountEuro;
     }
-    public float chf(float amount) {
+    public BigDecimal chf(float amount) {
         System.out.println("Type actual currency of CHF: ");
         chfCurrency = sc.nextFloat();
         chfCurrency = 1/chfCurrency;
-        amountChf = amount * chfCurrency;
+        amountChf = BigDecimal.valueOf(amount * usdCurrency).setScale(2, RoundingMode.HALF_UP);
         return amountChf;
     }
 }
@@ -72,18 +81,47 @@ public class main2 {
         //display amount
         System.out.println("Wallet amount: ");
         System.out.println(amount.userAmount);
-        //currency USD
-        currConv.usd(amount.userAmount);
-        System.out.println("Amount of wallet in USD: ");
-        System.out.println(currConv.amountUsd);
-        //currency EURO
-        currConv.euro(amount.userAmount);
-        System.out.println("Amount of wallet in Euro: ");
-        System.out.println(currConv.amountEuro);
-        //currency CHF
-        currConv.chf(amount.userAmount);
-        System.out.println("Amount of wallet in Chf: ");
-        System.out.println(currConv.amountChf);
-        
+
+        currConv.list();
+        System.out.println("Choose currency to calculate your amount of wallet: ");
+        int currencyChoose = sc.nextInt();
+        while (true) {
+            //currency USD
+            if (currencyChoose == 1) {
+                currConv.usd(amount.userAmount);
+                System.out.println("Amount of wallet in USD: ");
+                System.out.println(currConv.amountUsd);
+                currencyChoose = 0;
+            }
+            //currency EURO
+            if (currencyChoose == 2) {
+                currConv.euro(amount.userAmount);
+                System.out.println("Amount of wallet in Euro: ");
+                System.out.println(currConv.amountEuro);
+                currencyChoose = 0;
+            }
+            //currency CHF
+            if (currencyChoose == 3) {
+                currConv.chf(amount.userAmount);
+                System.out.println("Amount of wallet in Chf: ");
+                System.out.println(currConv.amountChf);
+                currencyChoose = 0;
+            }
+            //exit
+            if (currencyChoose == 4) {
+                break;
+            }
+            System.out.println("Do you want to choose other currency?");
+            System.out.println("1. Yes");
+            System.out.println("2. No");
+            int currNext = sc.nextInt();
+            if (currNext == 1) {
+                currConv.list();
+                System.out.println("Choose currency to calculate your amount of wallet: ");
+                currencyChoose = sc.nextInt();
+            } else if (currNext == 2) {
+                break;
+            } else System.out.println("Type again!");
+        }
     }
 }
